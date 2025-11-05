fd294a2b97304941aa66b6e0cc8874cbec04f0cc
bryan060807 <167461104+bryan060807@users.noreply.github.com>
2025-11-05 11:56:31 -0600

    fasg

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,7 +1,6 @@
 export { default as MusicLinks } from './MusicLinks';
 export { default as TestimonialsCarousel } from './TestimonialsCarousel';
 export { default as BandcampEmbed } from './BandcampEmbed';
-export { SongDisplay } from './SongDisplay';
 export { default as LogoutButton } from './LogoutButton';
 export { default as Login } from './Login';
 export { Welcome } from './Welcome';
