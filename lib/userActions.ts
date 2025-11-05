86d6b486790bd4a6e073474b3643355f32a3d95b
bryan060807 <167461104+bryan060807@users.noreply.github.com>
2025-11-05 12:00:03 -0600

    aga

--- lib/userActions.ts
+++ lib/userActions.ts
@@ -1,7 +1,7 @@
 'use server';
 
 import { revalidatePath } from 'next/cache';
-import { createSupabaseServerClient } from '@/lib/supabaseClient';
+import { createSupabaseServerClient } from '@/utils/supabase/server';
 
 interface ActionResponse {
   message: string;
